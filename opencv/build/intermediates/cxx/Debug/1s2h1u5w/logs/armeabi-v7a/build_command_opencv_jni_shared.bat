daf7b03e86d02b16c0fbb42148285d6b19cf28f9
yys-beep <yysyuen123@gmail.com>
2026-06-22 20:28:53 +0800

    update logo

---
+++ opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\win11\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\.cxx\\Debug\\1s2h1u5w\\armeabi-v7a" ^
+  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/armeabi-v7a/configure_command.bat
+++ opencv/build/intermediates/cxx/Debug/1s2h1u5w/logs/armeabi-v7a/configure_command.bat
@@ -5,15 +5,15 @@
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
   "-DANDROID_PLATFORM=android-21" ^
-  "-DANDROID_ABI=x86" ^
-  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
   "-DANDROID_NDK=C:\\Users\\win11\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
   "-DCMAKE_ANDROID_NDK=C:\\Users\\win11\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\win11\\AppData\\Local\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\win11\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\build\\intermediates\\cxx\\Debug\\1s2h1u5w\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\build\\intermediates\\cxx\\Debug\\1s2h1u5w\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\build\\intermediates\\cxx\\Debug\\1s2h1u5w\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\build\\intermediates\\cxx\\Debug\\1s2h1u5w\\obj\\armeabi-v7a" ^
   "-DCMAKE_BUILD_TYPE=Debug" ^
-  "-BC:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\.cxx\\Debug\\1s2h1u5w\\x86" ^
+  "-BC:\\Users\\win11\\AndroidStudioProjects\\green_lab\\opencv\\.cxx\\Debug\\1s2h1u5w\\armeabi-v7a" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
